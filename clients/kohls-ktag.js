3e60a4b9c5b6df8ad02a816af4f14f91d54985ec
ryrob30 <rrob@protonmail.com>
2021-06-11 10:43:38 -0700

    Add files via upload

---
+++ clients/kohls-ktag.js
@@ -0,0 +1,34 @@
+//RYAN version
+//removed visitorAPI.js, at.js
+function targetPageParamsAll() {
+	return {
+		"screenWidth": screen.width,
+		"screenHeight": screen.height,
+		"screenOrientation": screen.orientation.type,
+		"devicePixelRatio": window.devicePixelRatio
+	};
+}
+
+/*! ktag.js bundle */
+(function() {var _global = this||(1,eval)('this');$env = _global.$env||{};$env.builds = $env.builds||{};if (!$env.builds['ktag']) {$env.builds['ktag'] = 1617732715473;}if ($env.resource) $env.resource('ktag.js', {intact: false, loaded: (+new Date())});}).call((function() {return this;})());
+/*
+ADOBE VISITOR API
+removed -- Ryan
+*/
+
+/*!
+ * external/kohls.tag.js
+ * Code to go between visitor.js and at.js as part of ktag.js
+removed -- Ryan
+ */
+/**
+ * @license
+ * at.js 1.7.1 | (c) Adobe Systems Incorporated | All rights reserved
+ * zepto.js | (c) 2010-2016 Thomas Fuchs | zeptojs.com/license
+removed -- Ryan
+*/
+
+// ----- End of ktag.js bundle -----
+
+(function() {var resource = $env.resource('ktag.js');var now = (+new Date());resource.loaded = resource.loaded||now;resource.parsed = now;resource.intact = true;})();
+// ----- End of ktag.js bundle -----
